0cc6c319b96881baf6be7879e422a16e0e21a02f
Ruslan <rusik.arhireev@gmail.com>
2016-09-28 18:38:35 +0300

    New class Requester, which will take duties for http requests

--- js/rest_client.js
+++ js/rest_client.js
@@ -3,11 +3,10 @@ class ControllableObject {
 	constructor(id, actions, initial_status, type, description) {
 		this._id = id;
 		this._actions = actions;
-		this._initial_status = initial_status;
+		this._status = initial_status;
 		this._type = type;
 		this._description = description;
 	}
-
 }
 
 class Sunblind extends ControllableObject {
@@ -20,11 +19,9 @@ class Room {
 		this._description = description;
 		this._objects = new Map();
 	}
-
 	checkIfHas(object) {
 		return this._objects.has(object);
 	}
-
 	addObject(id, object) {
 		this._objects.set(object);
 	}
@@ -32,76 +29,82 @@ class Room {
 
 /*-------------------------------------------------------------------------------------*/
 
-
-function createRoom(_id, _desc) {
-	var row = document.createElement("div");
-	row.setAttribute("class", "accordion_row");
-
-	var ins_elem_id = document.createElement("div");
-	ins_elem_id.setAttribute("class", "ins-element-id");
-	var header_id = document.createElement("h4");
-	header_id.appendChild(document.createTextNode(_id));
-	ins_elem_id.appendChild(header_id);
-
-	var ins_elem_desc = document.createElement("div");
-	ins_elem_desc.setAttribute("class", "ins-element-desc");
-	var header_desc = document.createElement("h4");
-	var room_desc = document.createTextNode(_desc);
-	header_desc.appendChild(room_desc);
-	ins_elem_desc.appendChild(header_desc);
-
-	var clearing_div = document.createElement("div");
-	clearing_div.setAttribute("class", "clear");
-
-	row.appendChild(ins_elem_id);
-	row.appendChild(ins_elem_desc);
-	row.appendChild(clearing_div);
-
-	row.onclick = function() {
-
-		this.classList.toggle("active");
-		this.getElementsByClassName("room_objects_panel")[0].classList.toggle("show");
+var Requester = {
+	request: new XMLHttpRequest(),
+	url: 'http://localhost:10800',
+	getData: function(getting_type) {
+		request = Requester.request;
+		if (request !== undefined) {
+			request.open('GET',  Requester.url + getting_type, true);
+			request.onreadystatechange = function () {
+				if (request.readyState == 4) {
+					if (request.status == 200) {
+						console.log("It worked!");
+						return JSON.parse(request.responseText);
+					}
+					else {
+						console.log(request.status + ": " + request.statusText);
+					}
+				}
+			};
+			request.send(null);
+		}
 	}
-	return row;
-}
+};
+
+/*-------------------------------------------------------------------------------------*/
+
+var House = {
+	html_container: document.getElementById("rooms"),
+	room_bindings: new Map(),
+
+	init: function() {
+		var data = Requester.getData('/rooms/');
+		
+	},
+	createRoomDOMNode: function(_id, _desc) {
+		var row = document.createElement("div");
+		row.setAttribute("class", "accordion_row");
+
+		var ins_elem_id = document.createElement("div");
+		ins_elem_id.setAttribute("class", "ins-element-id");
+		var header_id = document.createElement("h4");
+		header_id.appendChild(document.createTextNode(_id));
+		ins_elem_id.appendChild(header_id);
+
+		var ins_elem_desc = document.createElement("div");
+		ins_elem_desc.setAttribute("class", "ins-element-desc");
+		var header_desc = document.createElement("h4");
+		var room_desc = document.createTextNode(_desc);
+		header_desc.appendChild(room_desc);
+		ins_elem_desc.appendChild(header_desc);
+
+		var clearing_div = document.createElement("div");
+		clearing_div.setAttribute("class", "clear");
+
+		row.appendChild(ins_elem_id);
+		row.appendChild(ins_elem_desc);
+		row.appendChild(clearing_div);
+
+		row.onclick = function() {
+			this.classList.toggle("active");
+			this.getElementsByClassName("room_objects_panel")[0].classList.toggle("show");
+		}
+		return row;
+	},
+	createRoomModel: function(id, desc) {
+		return new Room(id, desc);
+	},
+	bindModelWithDOM: function(htmlRow, roomModel) {
+		House.room_bindings.set(htmlRow, roomModel);
+	}
+};
+
+
 
 $(document).ready( function () {
-	/*
-	var data = $.ajax({
-		//url: 'http://api.ks-cube.tk/rooms/',
-		url: 'http://localhost:10800/rooms/',
-		//	crossDomain: true,
-		headers: {
-			'Access-Control-Allow-Origin': '*'
-		},
-		async: false,
-		dataType: 'json',
-		success: function(data){
-			console.log(data);
-		},
-		error: function(xhr, ajaxOptions, thrownError){
-			console.log(xhr.status);
-			console.log(ajaxOptions);
-			console.log(thrownError);
-		}
-	});
-	*/
-	var request = new XMLHttpRequest();
 
-	if (request.withCredentials !== undefined) {
-		request.open('GET', 'http://localhost:10800/rooms/', true);
-		//request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		//request.setRequestHeader("Connection", "close");
-		request.onreadystatechange = function () {
-			if (request.readyState == 4) {
-				console.log("It worked!");
-				var response = JSON.parse(request.responseText);
-				console.log(response.rooms[1]);
-				//console.log(request.responseText.rooms[0]);
-			}
-		};
-		request.send();
-	}
+	House.init();
 
 	var acc = document.getElementsByClassName("accordion_row");
 	var i;
@@ -116,7 +119,6 @@ $(document).ready( function () {
 	var container = document.getElementById("rooms");
 	var id = "R6";
 	var desc = String("Room for experiments");
-	console.log(desc);
-	container.appendChild(createRoom(id, desc));
+	container.appendChild(House.createRoomDOMNode(id, desc));
 });
 
